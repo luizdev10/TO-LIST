fce52b244abef3de8ec44f52176fb9b3e0e00684
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2025-11-27 17:50:45 -0300

    html e css completos / primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnAdd = document.getElementById('btn');
+const btnDt = document.getElementById('deleted');
+const btnCp = document.getElementById('completed');
+const input = document.getElementById('content-list');
+
+
+function AddItemList (){
+    
+}
+
+btnAdd.addEventListener('click', ()=>{
+    
+})
