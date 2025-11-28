f923bbf75c31658a6ec9e9c71cf58c14ba19f5c8
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2025-11-27 22:15:07 -0300

    add o script de add itens na lista

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 const btnAdd = document.getElementById('btn');
-const btnDt = document.getElementById('deleted');
-const btnCp = document.getElementById('completed');
+const btnDt = document.querySelector('.deleted');
+const btnCp = document.querySelector('.completed');
 const input = document.getElementById('content-list');
+const listCompleted = document.getElementById('area-item');
 
+let ListaDeTarefas = [];
 
-function AddItemList (){
+function AddNaList (){
+    ListaDeTarefas.push(input.value);
+    input.value ="";
+    mostraTarefa();
     
 }
+function mostraTarefa() {
+    let newLi = "";
 
-btnAdd.addEventListener('click', ()=>{
-    
-})
+    ListaDeTarefas.forEach(item =>{
+        newLi = newLi + `
+        <li class="item">
+                <div class="area-check completed">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-check2-circle" viewBox="0 0 16 16" >
+                        <path
+                            d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0" />
+                        <path
+                            d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
+                    </svg>
+                </div>
+                <div class="areaText">
+                    <p class="text-item">${item}</p>
+                </div>
+                <div class="area-check deleted">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-trash" viewBox="0 0 16 16" >
+                        <path
+                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                        <path
+                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                    </svg>
+                </div>
+            </li>
+        `
+    })
+
+    listCompleted.innerHTML = newLi;
+}
+btnAdd.addEventListener('click', AddNaList);
